8b776b98d857e9a1cb775111d9e541f8ed86505a
Лев <72322313+DamarooWork@users.noreply.github.com>
2020-12-09 04:53:36 +0300

    Add files via upload

---
+++ precache-manifest.ec177dbaf17067c9810f7ddab46a99d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df643098c01401b46626",
+    "url": "/respondents/css/app.b7ab9082.css"
+  },
+  {
+    "revision": "26a05165e8bb33d6f78485bbd8ec06dc",
+    "url": "/respondents/index.html"
+  },
+  {
+    "revision": "df643098c01401b46626",
+    "url": "/respondents/js/app.96726da3.js"
+  },
+  {
+    "revision": "f956b6d4676fa11dc7aa",
+    "url": "/respondents/js/chunk-vendors.c3b65eec.js"
+  },
+  {
+    "revision": "085e85ed9b7ad1fb5f9c2685d3c3deff",
+    "url": "/respondents/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/respondents/robots.txt"
+  }
+]);
